b411e94ba0f4fce708563ad4eeb44583dea93b04
KaarJoseph <85071917+KaarJoseph@users.noreply.github.com>
2023-05-03 09:01:09 -0500

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,54 @@
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './main.html',
+  styleUrls: ['./main.css']
+})
+export class App {
+  name = 'Angular';
+  cargo = 'Estudiante';
+
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+  contador: number = 0;
+
+  calcular() {
+    console.log('Hola UPS', this.numero1)
+  this.contador = this.contador + 1
+    this.resultado = this.numero1 + this.numero2; // Operación matemática
+  }
+
+  calcularR() {
+    console.log('Hola UPS', this.numero1)
+  this.contador = this.contador + 1
+    this.resultado = this.numero1 - this.numero2; // Operación matemática
+  }
+
+  calcularM() {
+    console.log('Hola UPS', this.numero1)
+  this.contador = this.contador + 1
+    this.resultado = this.numero1 * this.numero2; // Operación matemática
+  }
+}
+  /*
+  a = 0;
+  b = 0;
+  c = 0;
+  contador = 0;
+
+calcular(){
+  console.log('Hola UPS', this.a)
+  this.contador = this.contador + 1
+  this.c = this.a + eval(""+this.b)
+  }
+}*/
+
+bootstrapApplication(App);
